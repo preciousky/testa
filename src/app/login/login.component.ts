ea694c287b1d316916069d373d59430b122b2a0d
preciousky <1873685019@qq.com>
2019-11-01 14:02:26 +0800

    connect successfully

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../service/http.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from '../service/http.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,34 +11,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   testString: string;
   validateForm: FormGroup;
+
   constructor(
     private router: Router,
     private httpService: HttpService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder) {
+  }
 
-    ngOnInit(): void {
-      this.validateForm = this.fb.group({
-        userName: [null, [Validators.required]],
-        password: [null, [Validators.required]],
-        remember: [true]
-      });
-      this.testString = 'testing...';
-      }
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
+    this.testString = 'testing...';
+  }
 
-    login(): void {
-      for (const i in this.validateForm.controls) {
-        this.validateForm.controls[i].markAsDirty();
-        this.validateForm.controls[i].updateValueAndValidity();
-      }
-      var body = JSON.stringify({
-        "test": 'test'
-      });
-      this.httpService.postData('login', body)
-        .subscribe(data => {
+  login(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+    var body = JSON.stringify({
+      "test": 'test'
+    });
+    this.httpService.postData('login/', body)
+      .subscribe(data => {
           console.log('>>>>>>>>>>>>  received data >>>>>>>>>>>>>>>>>>>');
           console.log(data);
-          if (data['code'] == 1) {
-              this.testString = "test successfully!!!"
+          if (data['code'] != null) {
+            this.testString = "test successfully!!!"
           }
           console.log('######################link finish############################');
         },
@@ -46,5 +48,5 @@ export class LoginComponent implements OnInit {
           console.log(error);
         }
       );
-    }
+  }
 }
